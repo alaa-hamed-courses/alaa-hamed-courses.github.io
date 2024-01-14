8e3be5f8ea23e05ff53136ad4e32b7242d127af8
myfirstaccoun <85413282+myfirstaccoun@users.noreply.github.com>
2024-01-14 21:27:22 +0200

    Update theInfo.js

--- routes/theInfo.js
+++ routes/theInfo.js
@@ -4,7 +4,6 @@ let دورة = "";
 let fs = require('fs');
 let path = require('path');
 let coursesPath = path.join(__dirname, `../public`);
-let coursesFilesPath;
 let playlistID = [];
 let namesFiles = [];
 let startVideoInArr = [];
@@ -12,20 +11,60 @@ let isTests = [];
 let isError = [];
 let reload = 0; // is page reloaded
 
+// اعدادات
+router.use(express.json()); 
+
+const cors = require('cors');
+router.use(cors());
+
+// إعداد قاعدة البيانات SQLite
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:');
+
+// إنشاء جدول لتخزين البيانات
+db.serialize(() => {
+    db.run("CREATE TABLE IF NOT EXISTS shared_data (course TEXT PRIMARY KEY, active_lesson INTEGER)");
+
+    // // إضافة بيانات تجريبية إلى الجدول
+    // const initialData = [
+    //     "قيمة 1",
+    //     "قيمة 2",
+    //     "قيمة 3"
+    // ];
+
+    // const insertStatement = db.prepare("INSERT INTO shared_data (data) VALUES (?)");
+
+    // for (const value of initialData) {
+    //     insertStatement.run(value);
+    // }
+
+    // insertStatement.finalize();
+});
+
+
 let courses = [];
-fs.readdir(coursesPath, (err, folders) => {
-    for(let i = 0; i < folders.length; i++) {
+let myData = [];
+fs.readdir(coursesPath, async (err, folders) => {
+    await deleteDataFromDatabase();
+
+    for (let i = 0; i < folders.length; i++) {
         let str = folders[i];
-        if(str != "صور") {
+        if (str !== "صور") {
             courses.push(str.trim());
-            
-            coursesFilesPath = path.join(__dirname, `../public/${folders[i]}/الرابط.txt`);
-            fs.readFile(coursesFilesPath, "utf8", (err, data) => {
-                startVideoInArr.push(data.split("\r\n").length > 1? parseInt(data.split("\r\n")[1]) : 0);
-                data.split("\r\n").length > 2 && data.split("\r\n")[2] != ""? namesFiles.push([folders[i], 0]) : namesFiles.push([folders[i], 1]);
-                playlistID.push([folders[i], data.split("\r\n")[0]]);
-            });
+            myData.push({ "course": str.trim(), "active_lesson": 1 });
+
+            const coursesFilesPath = path.join(__dirname, `../public/${folders[i]}/الرابط.txt`);
+            const data = fs.readFileSync(coursesFilesPath, "utf8");
+
+            startVideoInArr.push(data.split("\r\n").length > 1 ? parseInt(data.split("\r\n")[1]) : 0);
+            data.split("\r\n").length > 2 && data.split("\r\n")[2] !== "" ? namesFiles.push([folders[i], 0]) : namesFiles.push([folders[i], 1]);
+            playlistID.push([folders[i], data.split("\r\n")[0]]);
         }
+    }
+
+    // push data to table
+    for (let i = 0; i < myData.length; i++) {
+        await saveDataToDatabase(myData[i]["course"], myData[i]["active_lesson"]);
     }
 });
 
@@ -60,6 +99,152 @@ function search2D(arr, item, column_search = 0, column_result = 1) {
     return res;
 }
 
+function searchInObjArr(arr, key_want_search, value_want_search) {
+    const res = [];
+    const ind = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        const item = arr[i];
+
+        if (typeof item === 'object' && item !== null) {
+            if (key_want_search in item && item[key_want_search] == value_want_search) {
+                res.push(item);
+                ind.push(i);
+            }
+        }
+    }
+
+    return [res, ind];
+}
+
+// البينات
+router.get('/get-data', (req, res) => {
+    // تعيين رأس الاستجابة لتحديد نوع المحتوى
+    res.setHeader('Content-Type', 'application/json');
+
+    // استعراض البيانات المخزنة في قاعدة البيانات
+    db.all("SELECT * FROM shared_data", (err, rows) => {
+        if (err) {
+            return res.status(500).send(err.message);
+        }
+
+        // إرجاع البيانات بتنسيق JSON
+        res.json(rows);
+    });
+});
+
+router.post('/save-data', (req, res) => {
+    const { course, active_lesson } = req.body;
+
+    // حفظ البيانات في قاعدة البيانات
+    db.run("INSERT OR REPLACE INTO shared_data (course, active_lesson) VALUES (?, ?)", [course, active_lesson], function(err) {
+        if (err) {
+            return res.status(500).send(err.message);
+        }
+
+        const savedDataId = this.lastID;
+        res.send(`تم حفظ البيانات بنجاح بالرقم: ${savedDataId}`);
+    });
+});
+
+router.put('/update-data/:course', (req, res) => {
+    const courseToUpdate = req.params.course;
+    const { active_lesson } = req.body;
+
+    // التحقق مما إذا كانت الدورة موجودًا
+    db.get("SELECT * FROM shared_data WHERE course = ?", [courseToUpdate], (err, row) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+
+        if (row) {
+            // إذا كانت الدورة موجودًا، يتم تحديث البيانات
+            db.run("UPDATE shared_data SET active_lesson = ? WHERE course = ?", [active_lesson, courseToUpdate], function(err) {
+                if (err) {
+                    return res.status(500).json({ error: err.message });
+                }
+
+                res.json({ message: `تم تحديث البيانات بنجاح` });
+            });
+        } else {
+            // إذا كانت الدورة غير موجود، يتم إنشاء سجل جديد
+            db.run("INSERT INTO shared_data (course, active_lesson) VALUES (?, ?)", [courseToUpdate, active_lesson], function(err) {
+                if (err) {
+                    return res.status(500).json({ error: err.message });
+                }
+
+                const newRecordId = this.lastID;
+                res.json({ message: `تم إنشاء سجل جديد بالرقم: ${newRecordId}` });
+            });
+        }
+    });
+});
+
+router.delete('/delete-data', (req, res) => {
+    db.run("DELETE FROM shared_data", (err) => {
+        if (err) {
+            return res.status(500).send(err.message);
+        }
+
+        res.send("تم حذف البيانات بنجاح");
+    });
+});
+
+// دوال البينات
+async function getDataFromServer() {
+    return new Promise((resolve, reject) => {
+        db.all('SELECT * FROM shared_data', (err, rows) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(rows);
+            }
+        });
+    });
+}
+
+async function saveDataToDatabase(course, lesson) {
+    return new Promise((resolve, reject) => {
+        db.run('INSERT INTO shared_data (course, active_lesson) VALUES (?, ?)', [course, lesson], function(err) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+async function updateDataInDatabase(course, lesson) {
+    return new Promise((resolve, reject) => {
+        db.run('UPDATE shared_data SET active_lesson = ? WHERE course = ?', [lesson, course], function(err) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+    });
+}
+
+function deleteDataFromDatabase() {
+    return new Promise((resolve, reject) => {
+        // تنفيذ عملية حذف البيانات في قاعدة البيانات
+        db.run("DELETE FROM shared_data", (err) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve("تم حذف البيانات بنجاح");
+            }
+        });
+    });
+}
+
+
+
+
+
+// الصفحات
 router.get("/", (req, res) => {
     res.redirect("/all-courses");
 });
@@ -68,150 +253,154 @@ router.get("/all-courses", (req, res) => {
     res.render("courses", {courses: courses, playlistID: playlistID, namesFiles: namesFiles});
 });
 
-router.get("/all-courses/:id", (req, res) => {
+router.get("/all-courses/:course", (req, res) => {
+    getDataFromServer().then((data) => {
+        let course = req.params.course;
+        res.redirect(`/all-courses/${course}/${searchInObjArr(data, "course", course)[0][0]["active_lesson"]}`);
+    });
+});
+
+router.get("/all-courses/:course/:id", (req, res) => {
     isTests = [];
-    دورة != ""? reload = (دورة == req.params.id? 1 : 0) : "";
+    دورة != ""? reload = (دورة == req.params.course? 1 : 0) : "";
 
-    دورة = req.params.id;
+    دورة = req.params.course;
     isError = [];
-    
-    let courseInterval = setInterval(() => {
-    if(دورة == req.params.id) {
-    let voicePath = path.join(__dirname, `../public/${دورة}/صوت`);
-    let pdfPath = path.join(__dirname, `../public/${دورة}/تفريغ وتشجير`);
-    let filesPath = path.join(__dirname, `../public/${دورة}`);
-    let a = "";
+    updateDataInDatabase(دورة, parseInt(req.params.id)).then((update_data) => {
+        let courseInterval = setInterval(() => {
+            if(دورة == req.params.course) {
+                let voicePath = path.join(__dirname, `../public/${دورة}/صوت`);
+                let pdfPath = path.join(__dirname, `../public/${دورة}/تفريغ وتشجير`);
+                let filesPath = path.join(__dirname, `../public/${دورة}`);
+                let a = "";
 
-    // tmp vars, cont ==> continue
-    let contTests = 0;
-    let cont = [["names", 0], ["pdf", 0], ["tests", 0]];
+                // tmp vars, cont ==> continue
+                let contTests = 0;
+                let cont = [["names", 0], ["pdf", 0], ["tests", 0]];
 
-    // names
-    let names = [];
-    fs.readdir(voicePath, (err, files) => {
-        a = files;
-        if(files != undefined) {
-            for(let i = 0; i < files.length; i++) {
-                let str = files[i];
-                str = right(str, str.length-5).split(".")[0];
-                names.push(str.trim());
-            }
-        } else {isError.push(1);}
-
-        cont[0][1] = 1;
-    });
-
-    // pdf
-    let pdf = [];
-    fs.readdir(pdfPath, (err, files) => {
-        if(files != undefined) {
-            for(let i = 0; i < files.length; i++) {
-                let str = files[i];
-                str = right(str, str.length-5).split(".")[0].split(" - ");
-                str != "op"? pdf.push(str) : "";
-            }
-        } else {isError.push(2);}
-
-        cont[1][1] = 1;
-    });
-    
-    let waitInterval = setInterval(() => {
-        if(cont[0][1] == 1 && cont[1][1] == 1) {
-            if(namesFiles[courses.indexOf(دورة)] == 0) {
-                names = [];
-                pdf = [];
-                isError = 0;
-            }
-
-            clearInterval(waitInterval);
-        }
-    })
-    
-    
-
-    let contIsTests = 0;
-
-    // isTest
-    let isTestInterval = setInterval(() => {
-        if(دورة == req.params.id) {
-            fs.readdir(filesPath, (err, files) => {
-                if(files != undefined) {
-                    for(let i = 0; i < files.length; i++) {
-                        let str = files[i];
-                        if(str.split(".txt")[0] == "اختبارات") {
-                            isTests.push(1);
+                // names
+                let names = [];
+                fs.readdir(voicePath, (err, files) => {
+                    a = files;
+                    if(files != undefined) {
+                        for(let i = 0; i < files.length; i++) {
+                            let str = files[i];
+                            str = right(str, str.length-5).split(".")[0];
+                            names.push(str.trim());
                         }
-                    }
-                } else {isError.push(3);}
+                    } else {isError.push(1);}
 
-                contIsTests = 1;
-                
-            });
-
-            clearInterval(isTestInterval);
-        }
-    });
-
-    
-    let tests = [];
-    let theTestsInterval = setInterval(() => {
-        if(contIsTests == 1 && cont[2][1] == 0 && دورة == req.params.id) {
-            if(isTests.length > 0) {
-                testsPath = path.join(__dirname, `../public/${دورة}/اختبارات.txt`);
-                fs.readFile(testsPath, "utf8", (err, data) => {
-                    let waitInterval = setInterval(() => {
-                        if(data != undefined) {
-                            for(let i = 0; i < data.split("\r\n").length; i++) {
-                                let test = data.split("\r\n")[i];
-                                tests.push([test.split("- ")[0], test.split("- ")[1]]);
-                            }
-
-                            contTests = 1;
-
-                            clearInterval(waitInterval);
-                        }
-                    });
+                    cont[0][1] = 1;
                 });
-            } else {contTests = 1;}
 
-            cont[2][1] = 1;
-            clearInterval(theTestsInterval);
-        }
-    });
+                // pdf
+                let pdf = [];
+                fs.readdir(pdfPath, (err, files) => {
+                    if(files != undefined) {
+                        for(let i = 0; i < files.length; i++) {
+                            let str = files[i];
+                            str = right(str, str.length-5).split(".")[0].split(" - ");
+                            str != "op"? pdf.push(str) : "";
+                        }
+                    } else {isError.push(2);}
 
-    let lessonInfo = [];
-    let testsInterval = setInterval(() => {
-        if(contTests == 1) {
-            lessonInfo = check2Ds(names, pdf, 1, 0);
-            // lessonInfo ==> [الخ ... "اسم الدرس", ["تفريغ", "كتاب"]]
-            // console.log([names[15], pdf[52][1]], names[15] == pdf[52][1]); // عند ظهور مشاكل في اختلاف التسمية - اسم الملف
-            isError.length == 0? res.render("index", {fff: a, lesInfo: JSON.stringify(lessonInfo), course: دورة, courses: courses, playlistID: playlistID, startVideoIn: startVideoInArr, tests: tests, namesFiles: namesFiles, reload: reload}) : res.status(404).send(`
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>خطأ</title>
-                <link data-default-icon="/public/صور/شعار.png" data-badged-icon="/public/صور/شعار.png" rel="shortcut icon" type="image/x-icon" href="/public/صور/شعار.png">
-            </head>
-            <body>
-                <h1>هذه الدورة غير موجودة</h1>
-            </body>
-            <script>
-                console.log("isError = [" + ${JSON.stringify(isError.sort((x, y) => x - y))} + "]");
-                setTimeout(() => {
-                    window.location.href = "/";
-                }, 5000);
-            </script>
-            </html>
-            `);
+                    cont[1][1] = 1;
+                });
+                
+                let waitInterval = setInterval(() => {
+                    if(cont[0][1] == 1 && cont[1][1] == 1) {
+                        if(namesFiles[courses.indexOf(دورة)] == 0) {
+                            names = [];
+                            pdf = [];
+                            isError = 0;
+                        }
 
-            clearInterval(testsInterval);
-        }
-    });
+                        clearInterval(waitInterval);
+                    }
+                })
+                
+                // isTest
+                let contIsTests = 0;
+                let isTestInterval = setInterval(() => {
+                    if(دورة == req.params.course) {
+                        fs.readdir(filesPath, (err, files) => {
+                            if(files != undefined) {
+                                for(let i = 0; i < files.length; i++) {
+                                    let str = files[i];
+                                    if(str.split(".txt")[0] == "اختبارات") {
+                                        isTests.push(1);
+                                    }
+                                }
+                            } else {isError.push(3);}
 
-    clearInterval(courseInterval);
-    }
+                            contIsTests = 1;
+                            
+                        });
+
+                        clearInterval(isTestInterval);
+                    }
+                });
+
+                let tests = [];
+                let theTestsInterval = setInterval(() => {
+                    if(contIsTests == 1 && cont[2][1] == 0 && دورة == req.params.course) {
+                        if(isTests.length > 0) {
+                            testsPath = path.join(__dirname, `../public/${دورة}/اختبارات.txt`);
+                            fs.readFile(testsPath, "utf8", (err, data) => {
+                                let waitInterval = setInterval(() => {
+                                    if(data != undefined) {
+                                        for(let i = 0; i < data.split("\r\n").length; i++) {
+                                            let test = data.split("\r\n")[i];
+                                            tests.push([test.split("- ")[0], test.split("- ")[1]]);
+                                        }
+
+                                        contTests = 1;
+
+                                        clearInterval(waitInterval);
+                                    }
+                                });
+                            });
+                        } else {contTests = 1;}
+
+                        cont[2][1] = 1;
+                        clearInterval(theTestsInterval);
+                    }
+                });
+
+                // open course
+                let lessonInfo = [];
+                let testsInterval = setInterval(() => {
+                    if(contTests == 1) {
+                        lessonInfo = check2Ds(names, pdf, 1, 0);
+                        // lessonInfo ==> [الخ ... "اسم الدرس", ["تفريغ", "كتاب"]]
+                        isError.length == 0? res.render("index", {fff: a, lesInfo: JSON.stringify(lessonInfo), course: دورة, courses: courses, playlistID: playlistID, startVideoIn: startVideoInArr, tests: tests, namesFiles: namesFiles, reload: reload}) : res.status(404).send(`
+                        <!DOCTYPE html>
+                        <html lang="en">
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <title>خطأ</title>
+                            <link data-default-icon="/public/صور/شعار.png" data-badged-icon="/public/صور/شعار.png" rel="shortcut icon" type="image/x-icon" href="/public/صور/شعار.png">
+                        </head>
+                        <body>
+                            <h1>هذه الدورة غير موجودة</h1>
+                        </body>
+                        <script>
+                            console.log("isError = [" + ${JSON.stringify(isError.sort((x, y) => x - y))} + "]");
+                            setTimeout(() => {
+                                window.location.href = "/";
+                            }, 5000);
+                        </script>
+                        </html>
+                        `);
+
+                        clearInterval(testsInterval);
+                    }
+                });
+
+                clearInterval(courseInterval);
+            }
+        });
     });
 });
 
